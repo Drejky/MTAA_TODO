9980d053a12007ea296709cbc0ca13621d5af32b
xhnilic3 <j.hnilica147@gmail.com>
2021-03-23 20:02:48 +0100

    DELETE users

--- TODO_list/users/views.py
+++ TODO_list/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 import psycopg2
+from django.views.decorators.csrf import csrf_exempt
 
 
 def dict_fetch_all(cursor):
@@ -12,22 +13,44 @@ def dict_fetch_all(cursor):
     ]
 
 
-# Create your views here.
-def handle_users(request, id):
-    if not id.isnumeric():
-        return HttpResponse("Incorrect user id", status=404)
-    if request.method == 'GET':
+# @csrf_exempt
+def get_user(request, id):
+    conn = psycopg2.connect(database='mtaa', user='postgres',
+                            password='postgres',
+                            host='localhost', port="5432")
+    print("Database opened successfully")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM users WHERE user_id={};".format(id))
+    rows = dict_fetch_all(cur)
+    conn.commit()
+    conn.close()
+
+    if len(rows) == 0:  # if there is not such a user (by id)
+        return HttpResponse("User not found", status=404)
+
+    return JsonResponse(rows[0], safe=False, status=200)
+
+def delete_user(request, id):
+    if get_user(request, id).status_code is not 200:
+        return HttpResponse("User not found", status=404)
+    else:
         conn = psycopg2.connect(database='mtaa', user='postgres',
                                 password='postgres',
                                 host='localhost', port="5432")
         print("Database opened successfully")
         cur = conn.cursor()
-        cur.execute("SELECT * FROM users WHERE user_id={};".format(id))
-        rows = dict_fetch_all(cur)
+        cur.execute("DELETE  FROM users WHERE user_id={};".format(id))
         conn.commit()
         conn.close()
+    return HttpResponse("OK", status=200)
 
-        if len(rows) == 0:  # if there is not such a user (by id)
-            return HttpResponse("User not found", status=404)
+@csrf_exempt
+def handle_users(request, id):
+    if not id.isnumeric():
+        return HttpResponse("Incorrect user id", status=404)
 
-    return JsonResponse(rows[0], safe=False)
+    if request.method == 'GET':
+        return get_user(request, id)
+    elif request.method == 'DELETE':
+        return delete_user(request, id)
+
