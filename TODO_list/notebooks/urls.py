bf16ffead2bf05c4e7084a4cccec852023871512
Drejky <peter.kudela9@gmail.com>
2021-03-24 08:51:55 +0100

    Adding forgotten path

--- TODO_list/notebooks/urls.py
+++ TODO_list/notebooks/urls.py
@@ -3,11 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/notebook_types', views.get_types, name='get_types'),
     path('', views.handle_notebooks, name='handle_notebooks'),
     path('<id>', views.handle_notebook, name='handle_notebook'),
     path('<id>/notes/<note_id>', views.handle_notes, name='handle_notes'),
     path('<id>/notes/', views.handle_note, name='handle_note'),
-    
+    path('/notebook_types', views.get_types, name='get_types'),
 
 ]
