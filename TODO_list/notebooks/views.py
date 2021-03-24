50569bfa94bfd529df2bb6f910b4482024bac953
xhnilic3 <j.hnilica147@gmail.com>
2021-03-24 12:32:27 +0100

    formatting according to pep-8

--- TODO_list/notebooks/views.py
+++ TODO_list/notebooks/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 import psycopg2
-#from psycopg2.extras import RealDictCursor
+# from psycopg2.extras import RealDictCursor
 import json
 from django.views.decorators.csrf import csrf_exempt
 
@@ -11,9 +11,10 @@ def get_notebook(request, id, cur):
         SELECT * FROM public.notebooks WHERE notebook_id = {}) row;""".format(id))
     notebook = cur.fetchone()
     if notebook:
-        return JsonResponse(notebook[0], safe=False, status = 200)
+        return JsonResponse(notebook[0], safe=False, status=200)
     else:
-        return HttpResponse("Notebook with id doesn't exist", status = 404)
+        return HttpResponse("Notebook with id doesn't exist", status=404)
+
 
 def put_notebook(request, id, cur, conn):
     temp = get_notebook(request, id, cur)
@@ -27,26 +28,27 @@ def put_notebook(request, id, cur, conn):
         body['collaborator_id'] = 'null'
     if not body['notebook_icon']:
         body['notebook_icon'] = 'null'
-    
+
     cur.execute("""UPDATE public.notebooks
     SET notebook_name = '{}', label = '{}', 
     notebook_color = '{}', collaborator_id = {}, notebook_icon = {}, update_date = CURRENT_DATE
     WHERE notebook_id = {};""".format(body['notebook_name'], body['label'],
-    body['notebook_color'], body['collaborator_id'], body['notebook_icon'], id))
+                                      body['notebook_color'], body['collaborator_id'], body['notebook_icon'], id))
     conn.commit()
 
     return get_notebook(request, id, cur)
+
 
 def delete_notebook(request, id, cur, conn):
     temp = get_notebook(request, id, cur)
     if temp.status_code is not 200:
         return temp
-    
+
     cur.execute("""DELETE FROM public.notebooks WHERE notebook_id = {}""".format(id))
     conn.commit()
 
-    return HttpResponse("Succesfully deleted", status = 200)
-    
+    return HttpResponse("Succesfully deleted", status=200)
+
 
 def create_notebook(request, cur, conn):
     body = json.loads(request.body)
@@ -61,18 +63,20 @@ def create_notebook(request, cur, conn):
     notebook_name, label, notebook_color, update_date, collaborator_id,
     notebook_icon) VALUES ({}, CURRENT_DATE, {}, '{}', '{}', '{}',
     CURRENT_DATE, {}, '{}');""".format(body['creator_id'], body['notebook_type'],
-    body['notebook_name'], body['label'], body['notebook_color'],
-    body['collaborator_id'], body['notebook_icon']))
+                                       body['notebook_name'], body['label'], body['notebook_color'],
+                                       body['collaborator_id'], body['notebook_icon']))
     conn.commit()
 
-    return HttpResponse("Notebook succesfully created", status = 201)
+    return HttpResponse("Notebook succesfully created", status=201)
+
 
 def get_notebooks(request, cur):
     cur.execute("""SELECT row_to_json(row) FROM(SELECT * FROM public.notebooks) as row;""")
 
     notebooks = cur.fetchall()
     print(notebooks)
-    return JsonResponse(notebooks, safe=False, status = 200)
+    return JsonResponse(notebooks, safe=False, status=200)
+
 
 def get_note(request, id, note_id, cur):
     temp = get_notebook(request, id, cur)
@@ -84,9 +88,10 @@ def get_note(request, id, note_id, cur):
         note_id, id))
     note = cur.fetchone()
     if note:
-        return JsonResponse(note[0], safe=False, status = 200)
+        return JsonResponse(note[0], safe=False, status=200)
     else:
-        return HttpResponse("Note with id doesn't exist", status = 404)
+        return HttpResponse("Note with id doesn't exist", status=404)
+
 
 def put_note(request, id, note_id, cur, conn):
     temp = get_note(request, id, note_id, cur)
@@ -97,26 +102,28 @@ def put_note(request, id, note_id, cur, conn):
 
     cur.execute("""UPDATE public.notes
     SET name = '{}', note_content = '{}' WHERE note_id = {}""".format(
-    body['name'], body['note_content'], note_id))
+        body['name'], body['note_content'], note_id))
     conn.commit()
 
     return get_note(request, id, note_id, cur)
 
+
 def delete_note(request, id, note_id, cur, conn):
-    temp = get_note(request, id, note_id,cur)
+    temp = get_note(request, id, note_id, cur)
     if temp.status_code is not 200:
         return temp
-    
+
     cur.execute("""DELETE FROM public.notes WHERE note_id = {}""".format(note_id))
     conn.commit()
 
-    return HttpResponse("Succesfully deleted", status = 200)
+    return HttpResponse("Succesfully deleted", status=200)
+
 
 @csrf_exempt
 def get_types(request):
     conn = psycopg2.connect(database='mtaa', user='postgres',
-                                password='postgres',
-                                host='localhost', port="5432")
+                            password='postgres',
+                            host='localhost', port="5432")
     cur = conn.cursor()
 
     cur.execute("""SELECT row_to_json(row) FROM(SELECT * FROM public.notebook_types) as row;""")
@@ -125,13 +132,14 @@ def get_types(request):
 
     cur.close()
     conn.close()
-    return JsonResponse(types, safe=False, status = 200)
+    return JsonResponse(types, safe=False, status=200)
+
 
 @csrf_exempt
 def handle_notebooks(request):
     conn = psycopg2.connect(database='mtaa', user='postgres',
-                                    password='postgres',
-                                    host='localhost', port="5432")
+                            password='postgres',
+                            host='localhost', port="5432")
     cur = conn.cursor()
 
     if request.method == 'POST':
@@ -142,17 +150,18 @@ def handle_notebooks(request):
     cur.close()
     conn.close()
 
+
 @csrf_exempt
 def handle_notebook(request, id):
     conn = psycopg2.connect(database='mtaa', user='postgres',
-                                    password='postgres',
-                                    host='localhost', port="5432")
+                            password='postgres',
+                            host='localhost', port="5432")
     cur = conn.cursor()
 
     if id == 'notebook_types':
         return get_types(request)
     elif not id.isnumeric():
-        return HttpResponse("Incorrect id", status = 404)
+        return HttpResponse("Incorrect id", status=404)
 
     if request.method == "GET":
         return get_notebook(request, id, cur)
@@ -166,14 +175,15 @@ def handle_notebook(request, id):
     cur.close()
     conn.close()
 
+
 @csrf_exempt
 def handle_notes(request, id, note_id):
     conn = psycopg2.connect(database='mtaa', user='postgres',
-                                    password='postgres',
-                                    host='localhost', port="5432")
+                            password='postgres',
+                            host='localhost', port="5432")
     cur = conn.cursor()
     if (not id.isnumeric()) or (not note_id.isnumeric()):
-        return HttpResponse("Incorrect id", status = 404)
+        return HttpResponse("Incorrect id", status=404)
 
     if request.method == "GET":
         return get_note(request, id, note_id, cur)
@@ -187,14 +197,15 @@ def handle_notes(request, id, note_id):
     cur.close()
     conn.close()
 
+
 @csrf_exempt
 def handle_note(request, id):
     conn = psycopg2.connect(database='mtaa', user='postgres',
-                                password='postgres',
-                                host='localhost', port="5432")
+                            password='postgres',
+                            host='localhost', port="5432")
     cur = conn.cursor()
     if not id.isnumeric():
-        return HttpResponse("Incorrect id", status = 404)
+        return HttpResponse("Incorrect id", status=404)
 
     body = json.loads(request.body)
 
@@ -202,10 +213,10 @@ def handle_note(request, id):
     insert into public.notes (notebook_id, name, create_date, update_date,
 	note_type, note_content) values ({}, '{}', 
     CURRENT_DATE, CURRENT_DATE, {}, '{}');""".format(
-        body['notebook_id'], body['name'], body['note_type'], body['note_content'] 
+        body['notebook_id'], body['name'], body['note_type'], body['note_content']
     ))
 
     conn.commit()
     cur.close()
     conn.close()
-    return HttpResponse("Note succsesfully created", status = 200)
+    return HttpResponse("Note succsesfully created", status=200)
